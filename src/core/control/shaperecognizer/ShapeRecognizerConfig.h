7e379380c5c71c2dcb4af2af75e5b0dd0ccda42d
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2023-01-03 12:40:29 -0500

    Increase rectangle tolerance

--- src/core/control/shaperecognizer/ShapeRecognizerConfig.h
+++ src/core/control/shaperecognizer/ShapeRecognizerConfig.h
@@ -20,7 +20,7 @@
 #define CIRCLE_MAX_SCORE 0.10                        // max circle score for circle (ideal circle = 0)
 #define SLANT_TOLERANCE (5 * M_PI / 180)             // ignore slanting by +/- 5 degrees
 #define RECTANGLE_ANGLE_TOLERANCE (15 * M_PI / 180)  // angle tolerance in rectangles
-#define RECTANGLE_LINEAR_TOLERANCE 0.20              // vertex gap tolerance in rectangles
+#define RECTANGLE_LINEAR_TOLERANCE 0.50              // vertex gap tolerance in rectangles
 #define POLYGON_LINEAR_TOLERANCE 0.20                // vertex gap tolerance in closed polygons
 
 
